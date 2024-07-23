e8c7ca133a9b55aac388d19b8ed629039bff336a
Ryan O'Dea <70209371+ryan-odea@users.noreply.github.com>
2024-07-22 21:38:52 -0400

    update figs

--- EDA_Plots.R
+++ EDA_Plots.R
@@ -4,40 +4,42 @@ race <- fread("CRC_race.csv")
 age <- fread("CRC_age.csv")
 
 ggplot(sex[, sex := stringr::str_to_title(sex)], aes(x = year)) +
-  geom_line(aes(y = total_est, color = "Estimated Total")) +
+  geom_line(aes(y = est_total, color = "Estimated Total")) +
   facet_wrap(~sex) +
   geom_line(aes(y = N_ID, color = "Observed")) +
-  geom_ribbon(aes(ymin = total_li, ymax = total_ui, fill = "Estimated Total"), alpha = 0.1) +
+  geom_ribbon(aes(ymin = est_lower, ymax = est_upper, fill = "Estimated Total"), alpha = 0.1) +
   theme_bw() +
   labs(x = "Year", y = "N") +
   scale_color_manual(name = "Legend", values = c("Observed" = "blue", "Estimated Total" = "darkorange")) +
   scale_fill_manual(name = "Legend", values = c("Estimated Total" = "darkorange"), guide = "none") +
-  geom_point(aes(y = total_est, col = "Estimated Total"), alpha = 0.5) +
+  geom_point(aes(y = est_total, col = "Estimated Total"), alpha = 0.5) +
   geom_point(aes(y = N_ID, col = "Observed"), alpha = 0.5)
 ggsave("CRC_sex.png")
 
-ggplot(race[, race := stringr::str_to_title(race)], aes(x = year)) +
-  geom_line(aes(y = total_est, color = "Estimated Total")) +
+ggplot(race[, race := stringr::str_to_title(race)
+            ][, race := ifelse(race == "Aiother", "Other",
+                               ifelse(race == "Asianpi", "Asian/PI", race))], aes(x = year)) +
+  geom_line(aes(y = est_total, color = "Estimated Total")) +
   facet_wrap(~race, scales = "free") +
   geom_line(aes(y = N_ID, color = "Observed")) +
-  geom_ribbon(aes(ymin = total_li, ymax = total_ui, fill = "Estimated Total"), alpha = 0.1) +
+  geom_ribbon(aes(ymin = est_lower, ymax = est_upper, fill = "Estimated Total"), alpha = 0.1) +
   theme_bw() +
   labs(x = "Year", y = "N") +
   scale_color_manual(name = "Legend", values = c("Observed" = "blue", "Estimated Total" = "darkorange")) +
   scale_fill_manual(name = "Legend", values = c("Estimated Total" = "darkorange"), guide = "none") +
-  geom_point(aes(y = total_est, col = "Estimated Total"), alpha = 0.5) +
+  geom_point(aes(y = est_total, col = "Estimated Total"), alpha = 0.5) +
   geom_point(aes(y = N_ID, col = "Observed"), alpha = 0.5)
 ggsave("CRC_race.png")
 
 ggplot(age[, agegrp := gsub("_", "-", agegrp)], aes(x = year)) +
-  geom_line(aes(y = total_est, color = "Estimated Total")) +
+  geom_line(aes(y = est_total, color = "Estimated Total")) +
   facet_wrap(~agegrp, scales = "free") +
   geom_line(aes(y = N_ID, color = "Observed")) +
-  geom_ribbon(aes(ymin = total_li, ymax = total_ui, fill = "Estimated Total"), alpha = 0.1) +
+  geom_ribbon(aes(ymin = est_lower, ymax = est_upper, fill = "Estimated Total"), alpha = 0.1) +
   theme_bw() +
   labs(x = "Year", y = "N") +
   scale_color_manual(name = "Legend", values = c("Observed" = "blue", "Estimated Total" = "darkorange")) +
   scale_fill_manual(name = "Legend", values = c("Estimated Total" = "darkorange"), guide = "none") +
-  geom_point(aes(y = total_est, col = "Estimated Total"), alpha = 0.5) +
+  geom_point(aes(y = est_total, col = "Estimated Total"), alpha = 0.5) +
   geom_point(aes(y = N_ID, col = "Observed"), alpha = 0.5)
 ggsave("CRC_age.png")
