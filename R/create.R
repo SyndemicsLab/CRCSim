218c8ff71ecb3173b67be250ceda97c1d0b95618
Ryan O'Dea <70209371+ryan-odea@users.noreply.github.com>
2024-07-30 13:25:43 -0400

    Update create.R

--- R/create.R
+++ R/create.R
@@ -9,116 +9,54 @@
 #' @returns a data.table
 #' @export
 
-create.data <- function(n, correlate = FALSE, collapse = TRUE, groups){
+create.data <- function(n_individuals, n_captures, n_strata, p_captures, p_strata){
+  out <- future.apply::future_lapply(1:n_individuals, function(x){
+    captures <- create.capture(n_captures, p_captures)
+    strata <- create.strata(n_strata, p_strata)
+    out <- data.table(t(captures), strata = strata)
 
-  stats <- list()
-  for(i in 1:n){
-    race <- estimate.race()
-    sex <- estimate.sex()
-    age <- estimate.age()
-    capture <- estimate.captures(correlate = correlate)
+    return(out)
+  }, future.seed = 2024)
 
-    stats[[i]] <- c(race = race, sex = sex, agegrp = age, capture)
-
-  }
-  DT <- rbindlist(stats)
-  if(!collapse) return(DT)
-
-  cols <- colnames(DT)[!colnames(DT) %in% c("race", "sex", "agegrp")]
-  if(!missing(groups)) cols <- c(cols, groups)
-
-  DT <- DT[, .(N_ID = .N), by = cols]
-
+  DT <- data.table::rbindlist(out)
+  DT <- DT[, list(N_ID = .N), by = c(names(DT))]
   return(DT)
 }
 
-#' Draws race from a sample
+#' A function to sample from 1:n along probability \code{prob}
 #'
-#' @param race list: races to sample from
-#' @param prob list: sampling probabilities relative to \code{race}
-#' @param seed int: seeding
+#' @param n number of 'strata' to simulate
+#' @param prob probabilities of n - expects summation to 1
 #'
 #' @keywords internal
-estimate.race <- function(race = c("White", "Black", "Asian/PI", "Hispanic", "Other"),
-                          prob = c(.777, .0509, .00629, .0942, .0109)){
-  if(length(race) != length(prob)) stop("race and prob have different lengths")
-  out <- sample(race, 1, prob, replace = FALSE)
+
+create.strata <- function(n, prob) {
+  if(length(prob) != n) stop("Probability length differs from n")
+  if(sum(prob) != 1) warning("Probability does not sum to 1")
+
+  out <- sample(1:n, 1, prob = prob)
+
   return(out)
 }
 
-#' Draws sex from a sample
+#' A function to generate binomial flips along the probability specified
 #'
-#' @param sex list: sexes to sample from
-#' @param prob list: sampling probabilities relative to \code{sex}
-#' @param seed int: seeding
+#' @param n number of 'captures' to simulate
+#' @param prob list of probabilities respective to each \code{n}
 #'
+#' @importFrom stats rbinom
 #' @keywords internal
-estimate.sex <- function(sex = c("Male", "Female"),
-                         prob = c(.583, .386)){
-  if(length(sex) != length(prob)) stop("sex and prob have different lengths")
-  out <- sample(sex, 1, prob, replace = FALSE)
-  return(out)
-}
+create.capture <- function(n, prob){
+  if(length(prob) != n) stop("Probability length differs from n")
 
-#' Draws age from a sample of bins
-#'
-#' @param age list: age bins to sample from
-#' @param prob list: probabilities relative to \code{age}
-#' @param seed int: seeding
-#'
-#' @keywords internal
-estimate.age <- function(age = c("1_10", "11_20", "21_30", "31_40",
-                                 "41_50", "51_60", "61_70", "71_80",
-                                 "81_90", "91_100"),
-                         prob = c(.00295, .0271, .273,
-                                  .274, .194, .154, .0512,
-                                  .0129, .00568, .000681)){
-  if(length(age) != length(prob)) stop("age and prob have different lengths")
-  out <- sample(age, 1, prob, replace = FALSE)
-  return(out)
-}
-
-#' Draws captures as a series of binomial flips
-#'
-#' @param capture list: captures to sample from
-#' @param prob list: binomial probabilities relative to \code{capture}
-#' @param correlate bool: attempts correlation between captures
-#' @param corr.matrix matrix: correlation between \code{capture}
-#' @param seed int: seeding
-#'
-#' @importFrom MASS mvrnorm
-#'
-#' @keywords internal
-estimate.captures <- function(capture = c("APCD", "BSAS", "Casemix", "Death", "Matris", "PMP"),
-                              prob = c(.4887, .1467, .096, .0108, .0527, .205),
-                              correlate = FALSE,
-                              corr.matrix = matrix(c(
-                                1.00000000, 0.03464933, -0.09139326, -0.06406634, -0.10679959, -0.11561070,
-                                0.03464933, 1.00000000,  0.21020500, -0.06738847,  0.33085861,  0.05487056,
-                                -0.09139326, 0.21020500,  1.00000000, -0.08669687,  0.09550053, -0.00245505,
-                                -0.06406634, -0.06738847, -0.08669687,  1.00000000, -0.08148882, -0.04910034,
-                                -0.10679959, 0.33085861,  0.09550053, -0.08148882,  1.00000000,  0.11940396,
-                                -0.11561070, 0.05487056, -0.00245505, -0.04910034,  0.11940396,  1.00000000),
-                                ncol=6, byrow=TRUE)){
-  if(length(capture) != length(prob)) stop("capture and prob have different lengths")
-  out <- list()
-  if(correlate){
-    mv <- MASS::mvrnorm(1, mu = rep(0, length(capture)), Sigma = corr.matrix)
-    uni <- pnorm(mv)
-    for(i in seq_along(capture)){
-      out[[i]] <- rbinom(1, 1, uni[[i]])
-    }
-  } else {
-    for(i in seq_along(capture)){
-      out[[i]] <- rbinom(1, 1, prob[[i]])
-    }
+  out <- vector(length = n)
+  for(i in seq_along(1:n)){
+    out[i] <- rbinom(1, 1, prob[[i]])
   }
-  names(out) <- capture
+  names(out) <- paste0("capture_", 1:n)
+
   return(out)
 }
-
-
-
 
 
 
